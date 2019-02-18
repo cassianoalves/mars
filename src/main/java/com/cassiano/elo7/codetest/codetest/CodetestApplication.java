2f1f78c37e514f7278c0548d0e25097a8c920b29
Cassiano Alves <cassiano.alves@gmail.com>
2019-02-18 17:43:24 -0300

    Spring boot scaffolding

---
+++ src/main/java/com/cassiano/elo7/codetest/codetest/CodetestApplication.java
@@ -0,0 +1,13 @@
+package com.cassiano.elo7.codetest.codetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodetestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodetestApplication.class, args);
+	}
+
+}
