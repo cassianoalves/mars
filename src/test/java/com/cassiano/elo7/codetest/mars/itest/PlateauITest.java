7216fc9cdc50a0f3e9a6ee0c44b8be3ef78322d9
Cassiano Alves <cassiano.alves@gmail.com>
2019-02-18 19:18:53 -0300

    Adding basic integrated test for plateau creation

---
+++ src/test/java/com/cassiano/elo7/codetest/mars/itest/PlateauITest.java
@@ -0,0 +1,45 @@
+package com.cassiano.elo7.codetest.mars.itest;
+
+import com.cassiano.elo7.codetest.mars.CodetestApplication;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {CodetestApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class PlateauITest {
+    @LocalServerPort
+    private int port;
+
+    @Before
+    public void setUp() {
+        RestAssured.port = port;
+    }
+
+    @Test
+    public void should_create_a_new_plateau_and_return_created_resource_at_Location_header() {
+        Response response =
+            given()
+            .body("{" +
+                    "\"xSize\": 5," +
+                    "\"ySize\": 10" +
+                    "}")
+            .when()
+            .post("/plateau")
+            .then()
+            .statusCode(HttpStatus.SC_CREATED)
+            .extract().response();
+
+        assertTrue(response.getHeader("Location").matches("http://localhost:" + port + "/plateau/[a-f,0-9]{32}]"));
+    }
+
+}
