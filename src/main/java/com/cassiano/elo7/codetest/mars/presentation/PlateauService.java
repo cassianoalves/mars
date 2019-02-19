3232ee199dd892805c293c0b63de67d56703f756
Cassiano Alves <cassiano.alves@gmail.com>
2019-02-19 11:56:47 -0300

    Implementing GET method

--- src/main/java/com/cassiano/elo7/codetest/mars/presentation/PlateauService.java
+++ src/main/java/com/cassiano/elo7/codetest/mars/presentation/PlateauService.java
@@ -4,12 +4,10 @@ import com.cassiano.elo7.codetest.mars.business.component.PlateauComponent;
 import com.cassiano.elo7.codetest.mars.business.entity.Plateau;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.websocket.server.PathParam;
 import java.net.URI;
 import java.net.URISyntaxException;
 
@@ -23,6 +21,11 @@ public class PlateauService {
     public ResponseEntity<Plateau> createPlateau(@RequestBody Plateau newPlateau, HttpServletRequest request) throws URISyntaxException {
         Plateau createdPlateau = plateauComponent.save(newPlateau);
         return ResponseEntity.created(buildLocation(createdPlateau, request)).body(createdPlateau);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Plateau> findPlateauById(@PathParam("id") String id) {
+        return ResponseEntity.ok(plateauComponent.findById(id));
     }
 
     private URI buildLocation(Plateau plateau, HttpServletRequest request) throws URISyntaxException {
