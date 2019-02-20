5cb77f99363a7f16e0bcd0dbc4723a73d33dac98
Cassiano Alves <cassiano.alves@gmail.com>
2019-02-20 01:06:34 -0300

    Implementing probe repo

--- src/main/java/com/cassiano/elo7/codetest/mars/integration/ProbeRepository.java
+++ src/main/java/com/cassiano/elo7/codetest/mars/integration/ProbeRepository.java
@@ -3,23 +3,35 @@ package com.cassiano.elo7.codetest.mars.integration;
 import com.cassiano.elo7.codetest.mars.business.entity.Probe;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Repository
 public class ProbeRepository {
-    public void deleteAll() {
+    private Map<String, Probe> table;
 
+    public ProbeRepository() {
+        table = new HashMap<>();
+    }
+
+    public void deleteAll() {
+        table.clear();
     }
 
     public Probe save(Probe probe) {
-        return null;
+        table.put(probe.getId(), probe);
+        return probe;
     }
 
     public Probe findById(String probeId) {
-        return null;
+        return table.get(probeId);
     }
 
-    public List<Probe> findAllByPlateau(String anyString) {
-        return null;
+    public List<Probe> findAllByPlateau(String plateauId) {
+        return table.values().stream()
+                .filter(p -> plateauId.equals(p.getContainingPlateau().getId()))
+                .collect(Collectors.toList());
     }
 }
