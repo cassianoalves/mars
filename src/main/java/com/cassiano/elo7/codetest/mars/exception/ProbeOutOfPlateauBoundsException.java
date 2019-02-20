98d7912aa43ee10b64e8bc066f6975019cb2a223
Cassiano Alves <cassiano.alves@gmail.com>
2019-02-20 01:38:59 -0300

    Testing probe movement

---
+++ src/main/java/com/cassiano/elo7/codetest/mars/exception/ProbeOutOfPlateauBoundsException.java
@@ -0,0 +1,9 @@
+package com.cassiano.elo7.codetest.mars.exception;
+
+import com.cassiano.elo7.codetest.mars.business.entity.Probe;
+
+public class ProbeOutOfPlateauBoundsException extends Error {
+    public ProbeOutOfPlateauBoundsException(Probe probe) {
+        super("Movement sends probe " + probe.getId() + " out of plateau " + probe.getContainingPlateau() + " bounds");
+    }
+}
--- src/test/java/com/cassiano/elo7/codetest/mars/business/component/ProbeMovementComponentTest.java
+++ src/test/java/com/cassiano/elo7/codetest/mars/business/component/ProbeMovementComponentTest.java
@@ -4,6 +4,8 @@ import com.cassiano.elo7.codetest.mars.business.entity.Direction;
 import com.cassiano.elo7.codetest.mars.business.entity.Plateau;
 import com.cassiano.elo7.codetest.mars.business.entity.Probe;
 import com.cassiano.elo7.codetest.mars.business.entity.ProbeCommand;
+import com.cassiano.elo7.codetest.mars.exception.ProbeOutOfPlateauBoundsException;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -15,13 +17,40 @@ import static com.cassiano.elo7.codetest.mars.business.entity.Direction.*;
 
 public class ProbeMovementComponentTest {
 
+    private ProbeMovementComponent probeMovementComponent;
+    private Plateau plateau;
+    private Probe probe;
 
+    @Before
+    public void setUp() {
+        probeMovementComponent = new ProbeMovementComponent();
+        plateau = new Plateau("aaa", 3, 3);
+        probe = new Probe("bbb", 0, 0, N, plateau);
+    }
 
     @Test
     public void should_move_probe_by_all_plateau() {
-        Plateau plateau = new Plateau("aaa", 3, 3);
-        Probe probe = new Probe("bbb", 0, 0, N, plateau);
-        List<ProbeCommand> commands = Arrays.asList(R, M);
+        List<ProbeCommand> commands = Arrays.asList(R, M, M, L, L, M, M, R, R, M, M);
+
+        Probe result = probeMovementComponent.move(probe, commands);
+
+        assertEquals(2, result.getPositionX());
+        assertEquals(2, result.getPositionY());
+        assertEquals(E, result.getDirection());
+    }
+
+    @Test(expected = ProbeOutOfPlateauBoundsException.class)
+    public void should_throw_exception_when_move_probe_reach_plateau_limit() {
+
+        List<ProbeCommand> commands = Arrays.asList(L, M, M);
+
+        Probe result;
+        try {
+            result = probeMovementComponent.move(probe, commands);
+        } catch (ProbeOutOfPlateauBoundsException e) {
+            assertEquals("Movement sends probe bbb out of plateau aaa bounds", e.getMessage());
+            throw e;
+        }
 
     }
 
